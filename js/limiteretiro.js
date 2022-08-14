7135e5b5946104b6a9af442dd83bcab00b0ec7e7
Juan Gustavo Granados Merchán <61571337+jgranados4@users.noreply.github.com>
2022-08-14 01:53:09 +0000

    otro cambios

--- js/limiteretiro.js
+++ js/limiteretiro.js
@@ -1,4 +1,21 @@
+let Monto =document.getElementById('Monto')
 let input = document.getElementById("montoEspeci");
 input.addEventListener("input", function () {
   if (this.value.length > 10) this.value = this.value.slice(0, 10);
 });
+const Habilitar = () => {
+  let val = 0;
+  if (input.value == "") {
+    val++;
+  }
+  if (Monto.value == "") {
+    val++;
+  }
+  if (val === 0) {
+    document.getElementById("guardar").disabled = false;
+  } else {
+    document.getElementById("guardar").disabled = true;
+  }
+};
+input.addEventListener("keyup", Habilitar);
+Monto.addEventListener("change", Habilitar);
